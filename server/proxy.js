79571fd8b1902d50d0004a7f60779cffa93e5c5a
Sathish Kumar <sathishkumar.aero51@gmail.com>
2021-05-15 18:08:33 +0530

    enable linting

--- server/proxy.js
+++ server/proxy.js
@@ -1,42 +1,42 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 
-var express = require('express'),
-request = require('request'),
-bodyParser = require('body-parser'),
-app = express();
+const app = express();
 
-var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+const myLimit = typeof (process.argv[2]) !== 'undefined' ? process.argv[2] : '100kb';
 console.log('Using limit: ', myLimit);
 
-app.use(bodyParser.json({limit: myLimit}));
+app.use(bodyParser.json({ limit: myLimit }));
 
-app.all('*', function (req, res, next) {
+app.all('*', (req, res) => {
+/** Set CORS headers: allow all origins, methods, and headers:
+ *  you may want to lock this down in a production environment */
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', req.header('access-control-request-headers'));
 
-// Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-
-if (req.method === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     // CORS Preflight
     res.send();
-} else {
-    var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
+  } else {
+    const targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
     if (!targetURL) {
-        res.status(500).send({ error: 'There is no Target-Endpoint header in the request' });
-        return;
+      res.status(500).send({ error: 'There is no Target-Endpoint header in the request' });
+      return;
     }
     request({ url: targetURL, method: req.method },
-        function (error, response, body) {
-            if (error) {
-                console.error('error: ' + response.statusCode)
-            }
-//                console.log(body);
-        }).pipe(res);
-}
+      (error, response) => {
+        if (error) {
+          console.error(`error: ${response.statusCode}`);
+        }
+        //                console.log(body);
+      }).pipe(res);
+  }
 });
 
 app.set('port', process.env.PORT || 3001);
 
-app.listen(app.get('port'), function () {
-console.log('Proxy server listening on port ' + app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log(`Proxy server listening on port ${app.get('port')}`);
 });
